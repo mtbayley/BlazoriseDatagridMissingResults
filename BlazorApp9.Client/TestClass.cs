29e66164ed8e68ade5202910f3ac3e51794ca85a
Michael Bayley <mbayley@innovativeautomation.com>
2024-01-15 14:49:23 -0500

    Add project files.

---
+++ BlazorApp9.Client/TestClass.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp9.Client;
+
+public class TestClass
+{
+    public required string EmployeeName { get; set; }
+    public DateTime Date { get; set; }
+    public double Hours { get; set; }
+}
---
+++ BlazorApp9/Program.cs
@@ -0,0 +1,42 @@
+using BlazorApp9.Client.Pages;
+using BlazorApp9.Components;
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddBlazorise()
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+app.Run();
